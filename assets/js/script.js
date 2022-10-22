5336b594c2f4c0887a147e09f0d4f199c283f3a5
Ajaws2022 <112523117+Ajaws2022@users.noreply.github.com>
2022-10-22 12:21:59 -0700

    began adding variables for api function in js

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// get the weather url and set it as a variable
+
+// create a function that combines the search word with the url using the split method
+// append the results to the forecast display and create a button with the search word
+// within the function set the search in local storage so that it can be shown later
+// 
+
+var APIKey = '7cb92b27a8129439217e5257f292a3e9';
+
+var city;
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+
