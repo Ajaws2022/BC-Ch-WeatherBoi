d9d4be9b6b796055fe142bc597cef9083d582df3
Ajaws2022 <112523117+Ajaws2022@users.noreply.github.com>
2022-10-23 17:02:39 -0700

    created functions to pull weather data and display it on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,34 +4,67 @@
 
 // within the function set the query as a button with a city name so that it can be called later
 // 
-var inputCity = document.getElementById('#cityName')
 
-var search = document.getElementById('#searchButton')
+
+var search = document.getElementById('searchButton')
 
 var APIKey = '7cb92b27a8129439217e5257f292a3e9';
+var currWeather = document.querySelector(".weather")
+var city = document.getElementById('cityName').value;
 
-var city;
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + 'Riverside' + "&appid=" + APIKey;
 
-var pastSearches = document.getElementsByClassName('.pastSearches')
+var pastSearches = document.getElementById('pastSearches') 
 
-pastSearches.textContent = 'Im alive';
+// function getApi(queryURL) {
+//     fetch(queryURL)
+//       .then(function (response) {
+//         console.log(response);
+//         if (response.status === 200) {
+//           pastSearches.textContent = `${response.status}`;
+//         }
+//         console.log(response.json());
+//          return response.body;
+//     });
+//   }
 
-console.log(pastSearches.textContent)
+//   getApi(queryURL)
 
-function getApi(queryURL) {
-    fetch(queryURL)
-      .then(function (response) {
-        console.log(response);
-        if (response.status === 200) {
-          pastSearches.textContent = `${response.status}`;
-        }
-        console.log(response.json());
-        return response.body;
-    });
-  }
+function fetchWeather(){
+  var inputCity = document.getElementById('cityName').value;
+  
+  var queryURL = 
+  "http://api.openweathermap.org/data/2.5/weather?q="+inputCity+"&units=imperial&appid="+APIKey;
 
-  getApi(queryURL)
+  fetch(
+    queryURL
+  ).then((response) => response.json())
+  .then((data)=> displayWeather(data));
 
-  console.log(pastSearches.textContent)
+    function displayWeather(data){
+       const { name } = data;
+       const { icon, description} = data.weather[0];
+       const { temp } = data.main;
+       const { humidity } = data.main;
+       const { speed } = data.wind;
+       console.log(name, icon, description, temp, humidity, speed)
+       document.querySelector(".city").innerHTML = "Weather in " + name;
+       document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + 
+       ".png";
+       document.querySelector(".temp").innerHTML = temp + "°F";
+       document.querySelector(".description").innerHTML = description;
+       document.querySelector(".humidity").innerHTML = "Humidity: " + humidity + "%";
+       document.querySelector(".wind").innerHTML = "Wind Speed: " + speed + " mph";
+
+    }
+
+}
+
+function searchByCity(){
+  var inputCity = document.getElementById('cityName').value;
+  fetchWeather(inputCity)
+}
+
+search.addEventListener('click', searchByCity)
+
+// console.log(fetchWeather('Riverside'))
