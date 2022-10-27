f51f0d4fbc3e31e52ae32b2161c6c7dd53d59b1a
Ajaws2022 <112523117+Ajaws2022@users.noreply.github.com>
2022-10-26 20:56:26 -0700

    updated loops to append forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,11 +60,66 @@ function fetchWeather(){
 
 }
 
+function fetchForecast(){
+  var inputCity = document.getElementById('cityName').value;
+  
+  var forecastURL = "http://api.openweathermap.org/data/2.5/forecast?q="+inputCity+"&units=imperial&cnt=5&appid="+APIKey
+
+  fetch(
+    forecastURL
+  ).then((response) => response.json())
+  .then((data)=> createForecast(data));
+  }
+  
+var lati;
+
+var longi;
+
+function getCoords(data){
+  var inputCity = document.getElementById('cityName').value;
+  var geoCode = "http://api.openweathermap.org/geo/1.0/direct?q=" + inputCity + "&limit=1&appid=" +APIKey
+  fetch(
+    geoCode
+  ).then((response) => response.json())
+   let { lat } = data
+   let { long } = data
+   lati.value = lat;
+   longi.value = long;
+}
+
+  function createForecast(data){
+  //  for(i=0; i<=5; i++){
+      var dayBox = document.getElementsByClassName('dayBox')
+
+      console.log(data.list)
+
+      for(i=0; i<5; i++){
+        
+        console.log(data.list[i]);
+        var forecastDay = data.list[i];
+        let { icon, description} = forecastDay.weather[0];
+        let { temp } = forecastDay.main;
+        let { humidity } = forecastDay.main;
+        let { speed } = forecastDay.wind;
+        let { dt } = forecastDay
+        console.log(temp,humidity,speed,description,icon)
+        console.log(dayBox[i])
+        let unixStamp = dt; 
+        let milliseconds = unixStamp * 1000;
+        let dateObject = new Date(milliseconds)
+        let trueDate = dateObject.toLocaleDateString("en-US")
+        dayBox[i].querySelector('h3').innerHTML= trueDate;
+        console.log(trueDate)
+      }
+
+  }
+
+
 function searchByCity(){
   var inputCity = document.getElementById('cityName').value;
   fetchWeather(inputCity)
 }
 
-search.addEventListener('click', searchByCity)
+search.addEventListener('click', searchByCity);
+search.addEventListener('click', fetchForecast);
 
-// console.log(fetchWeather('Riverside'))
